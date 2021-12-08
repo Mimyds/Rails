cf4e4240c29745f497049b604954ae4835ebe1e2
Mylene Desroses <mylene.desroses@gmail.com>
2021-12-08 13:31:13 -0400

    fin exo

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+class QuestionsController < ApplicationController
+
+  def ask
+
+  end
+
+  def answer
+    @answers = ["Great!", "Silly question, get dressed and go to work!", "I don’t care, get dressed and go to work!"]
+    if params[:question] == "I am going to work right now!"
+        @answers = @answers[0]
+      elsif params[:question].end_with?("?")
+        @answers = @answers[1]
+      else
+        @answers = @answers[2]
+    end
+  end
+end
